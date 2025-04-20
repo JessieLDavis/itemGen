982d886945c4d34c26287afe7cfe37a1bd6565c1
JessieLDavis <50719217+JessieLDavis@users.noreply.github.com>
2025-04-20 00:20:34 -0500

    day1_items

--- main.py
+++ main.py
@@ -53,16 +53,27 @@ materialList = {"animal":["antler","bone","coral","feather","hair","hide","horn"
 category = ["apparel","tool","product"]
 tagOpts = [
    ["protective equipment","medical equipment","combat equipment","textile equipment"],
-   ["entertainment device","communication device","illumination device","transportation device"],
+   ["entertainment device","communication device","illumination device","transportation device","musical device"],
    ["cultural item","stabbing item","scooping item","storage item","slashing item","decorative item"],
    ["dining tool","cutting tool","maker tool","prediction tool","investigative tool"]]
 objectHeader = ["category","name","materials","size","tags","purpose"]
 sizeOpts = ["any","small","medium","large"]
 # objectNames = []
 apparelObjects = {
-    "apron":{"materials":[("animal",["hide"]),("vegetal",["fibers","paper","reeds"]),("mineral",["synthetic"])],"size":[sizeOpts(0)],
-     "purpose":"Often used to prevent unwanted splashing.","tags":[tagOpts[0][1],tagOpts[0][0],tagOpts[3][4],tagOpts[3][0]]}
-}
+    "apron":{"materials":[("animal",["hide"]),("vegetal",["fibers","paper","reeds"]),("mineral",["synthetic"])],
+             "size":[sizeOpts(0)],
+             "purpose":"Often used to prevent unwanted splashing.",
+             "tags":[tagOpts[0][1],tagOpts[0][0],tagOpts[3][4],tagOpts[3][0]]},
+    "belt":{"materials":[("animal",["hide","shell","ivory","bone"]),("composite",["hide","metal"]),("human remains",["artifactual"]),("mineral",["ceramic","metal","synthetic"]),("vegetal",["fibers","reeds","wood"])],
+            "size":[sizeOpts[1],sizeOpts[2]],
+            "purpose":"Tool used in both technology and apparel to provide tension. In some cultures, unconventionally large versions were used as a reward for prowess on the battlefield.",
+            "tags":[tagOpts[0][2],tagOpts[2][3],tagOpts[2][0],tagOpts[2][5]]},
+    "bracelet":{"materials":[("animal",["bone","coral","feather","hair","hide","ivory","shell"]),("composite",["fibers","ceramic"]),("mineral",["ceramic","glass","metal","stone","synthetic"]),("vegetal",["fibers","paper","wood"])],
+            "size":[sizeOpts[1]],
+            "purpose":"An apparel item that humans often placed on their upper extremities for decorative purposes.",
+            "tags":[tagOpts[0][0],tagOpts[2][0],tagOpts[2][5],tagOpts[1][0]]}
+
+} #ceramic belt = sander?; purpose ref = WWE
 toolObjects = {
    "axe":{"materials":[("animal",["antler","bone","ivory","shell"]),("composite",["wood","metal"]),("mineral",["ceramic","metal","synthetic","stone"])],
           "size":[sizeOpts(1),sizeOpts(2)],
@@ -95,7 +106,11 @@ toolObjects = {
     "beaker":{"materials":[("mineral",["glass","synthetic"])],
               "size":[sizeOpts[1]],
               "purpose":"Storage item often used by human researchers.",
-              "tags":[tagOpts[0][1],tagOpts[2][0],tagOpts[2][3],tagOpts[3][4]]}
+              "tags":[tagOpts[0][1],tagOpts[2][0],tagOpts[2][3],tagOpts[3][4]]},
+    "bowl":{"materials":[("animal",["bone","ivory","shell","hide"]),("mineral",["ceramic","glass","metal","stone","synthetic"]),("vegetal",["paper","fibers","wood"])],
+            "size":[sizeOpts[0]],
+            "purpose":"Versatile item often used for dining, storage, and decoration.",
+            "tags":[tagOpts[2][0],tagOpts[2][3],tagOpts[2][5],tagOpts[3][0]]},
 
 }
 #add labrynth ref if glass ball
@@ -108,12 +123,25 @@ productObjects = {
             "size":[sizeOpts[1]],
             "purpose":"Decorative item often used when creating apparel. Generally stored in multiples.",
             "tags":[tagOpts[0][3],tagOpts[2][0],tagOpts[2][5],tagOpts[3][2]]},
+    "bell":{"materials":[("mineral",["metal"])],
+            "size":[sizeOpts[0]],
+            "purpose":"Instrument and cultural item often used to decorate apparel.",
+            "tags":[tagOpts[2][5],tagOpts[1][0],tagOpts[2][0],tagOpts[3][2]]},
+    "disk, coin":{"materials":[("mineral",["metal"])],
+            "size":[sizeOpts[1]],
+            "purpose":"A highly significant - but common - cultural disk used in commerce.",
+            "tags":[tagOpts[1][1],tagOpts[2][0],tagOpts[1][3],tagOpts[0][0]]},
 }
 
-# Beaker,Bell,Belt,# Bottle # Bow 
-# Bowl # Box 
-# Bracelet 
-# Brush # Blank, Disk #Disk (can be money)
+# "item":{"materials":[("",[""])],
+#             "size":[sizeOpts[]],
+#             "purpose":"",
+#             "tags":[tagOpts[][],tagOpts[][],tagOpts[][],tagOpts[][]]},
+
+
+
+# Bottle # Bow # Box 
+# Brush # Blank, Disk #Disk
 # Blanket 
 # Blouse 
 # Buckle # Building Material # Cord # Button 
@@ -205,346 +233,3 @@ productObjects = {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# {"abrader":{"modern":"sander","material":["mineral",("animal":"bone")],"purpose":"Long ago this tool was used to make items look more worn. This in some cultures made the human appear more desireable. Less frequently, versions of this tool were used to enpointify other tools. Recent studies suggest that these use-cases often had an inverse relationship throughout human history."},
-# "Adobe 
-# Adze Blade 
-# Adze Head 
-# Antler Artifact 
-# Apache Tear 
-# Armor Slats
-# Arrow Foreshaft 
-# Arrow Shaft 
-# Atlatl 
-# Atlatl Foreshaft 
-# Atlatl Shaft 
-# Bag Handle 
-# Bag, Apron 
-# Bag, Awl 
-# Bag, Pipe 
-# Basket, Burden 
-# Basketry Fragment 
-# Basketry Fragment, Cord  
-# Basketry Fragment, Sherd  
-# Baton 
-# Batten 
-# Beam 
-# Beamer 
-# Bell 
-# Belt 
-# Biface 
-# Blade 
-# Blank 
-# Blank, Disk 
-# Blanket 
-# Blouse 
-# Blubber Hook Prong 
-# Blubber Scraper 
-# Bola 
-# Bola Weight 
-# Bone Artifact 
-# Bottle 
-# Bottle, Water Bow 
-# Bow 
-# Bowl 
-# Bowl, Effigy 
-# Bowl, Rattle 
-# Bowl, Seed 
-# Bowl, Sherd 
-# Box 
-# Bracelet 
-# Brush 
-# Buckle 
-# Building Material 
-# Bull Roarer 
-# Bundle 
-# Bundle, Cord 
-# Burin 
-# Button 
-# Cane 
-# Cane Cigarette 
-# Canteen 
-# Canteen, Effigy 
-# Cauldron 
-# Censer 
-# Censer, Effigy 
-# Chisel 
-# Chopper 
-# Cigarette  
-# Clasp 
-# Clay Artifact 
-# Cleaver 
-# Club 
-# Colander 
-# Comb 
-# Concretion 
-# Container 
-# Coprolite 
-# Cord 
-# Cord/Bead 
-# Cordage 
-# Core 
-# Core, Chopper 
-# Core, Hammerstone 
-# Core Fragment 
-# Core/Microblade 
-# Core Tool 
-# Corn Husk Knot 
-# Corn Leaf Knot 
-# Cover 
-# Cover, Pot 
-# Cradle 
-# Cradle Board 
-# Cradle Board Belt  
-# Cradle Board Frame 
-# Crystal 
-# Cup 
-# Cylinder 
-
-
-# Dart, Atlatl  
-# Debitage 
-# Die 
-# Digging Stick 
-# Dipper 
-# Dipper Handle 
-# Dipper Sherd 
-# Discoid 
-# Discoid/Hammerstone 
-# Dish 
-# Disk 
-# Drill 
-# Endblade 
-# Effigy, Animal 
-# Effigy, Bird 
-# Effigy Jar, Horned Lizard 
-# Faunal Material 
-# Fetish 
-# Figurine 
-# Fire Drill 
-# Fire Drill Shaft 
-# Flake 
-# Flake, Retouched 
-# Flake Tool 
-# Flake, Utilized 
-# Flake, Waste 
-# Flaker 
-# Flesher 
-# Float 
-# Floral Material 
-# Flute 
-# Gaming Piece 
-# Gouge 
-# Gourd 
-# Gourd Jar 
-# Graver 
-# Grinding Slab 
-# Guard, Wrist 
-# Hairpin 
-# Hammerstone 
-# Handle 
-# Harpoon Foreshaft 
-# Harpoon Head 
-# Harpoon Part 
-# Harpoon Point 
-# Heddle Stick 
-# Hoe 
-# Hook 
-# Hoop 
-# Human Remains 
-# Ivory Artifact 
-# Jar 
-# Jar Base 
-# Jar, Cord 
-# Jar, Effigy 
-# Jar, Gourd 
-# Jar, Sherd  
-# Jar, Seed 
-# Jar, Seed, Bird Effigy 
-# Kiaha 
-# Kiaha Helping-stick 
-# Kilt 
-# Knife 
-# Knife Handle 
-# Knife, Tabular 
-# Knot 
-
-
-# Labret 
-# Ladder 
-# Ladle 
-# Ladle Rattle Handle 
-# Ladle, Effigy 
-# Lamp 
-# Leather Artifact 
-# Leister 
-# Lid 
-# Lime Container 
-# Line Weight 
-# Lintel 
-# Loom Anchor 
-# Loop 
-# Mano 
-
-# Mat 
-# Mat Fragment 
-# Mat/Basketry Fragment 
-# Mat/Cord 
-# Mattock Blade 
-# Maul 
-# Medal 
-# Medicine Box 
-# Medicine Stone 
-# Metate 
-# Microblade 
-# Moccasin 
-# Mug 
-# Nail 
-# Necklace 
-# Necklace/Bracelet 
-# Needle 
-# Needle Case 
-# Net 
-# Net Float 
-# Net, Burden 
-# Ojos de Dios 
-# Pad 
-# Paddle  
-# Palette 
-# Patch 
-# Pebble 
-# Pebble Tool 
-# Peg 
-# Pendant 
-# Pestle 
-# Pick 
-# Pick, Ice 
-# Pigment 
-# Pipe 
-# Pitcher 
-# Pitcher, Effigy 
-# Plank 
-# Plaque 
-# Plate 
-# Plate, Jar Base 
-# Plate, Legged 
-# Plate, Tripod 
-# Point, Bird 
-# Poncho 
-# Pot, Bird 
-# Pot Rest 
-# Pot Ring 
-# Pouch and Contents 
-# Prayer Feather 
-# Prayer Plume 
-# Prayer Stick 
-# Preform 
-# Projectile Point 
-# Punch 
-# Quid 
-# Quiver 
-# Rabbit Stick 
-# Rasp 
-# Rattle 
-# Raw Material 
-# Ring 
-# Ring Vessel 
-# Robe 
-# Robe/Belt 
-# Robe/Textile Fragment 
-# Rod 
-# Rope 
-# Rope/Cord 
-# Rope/Cord/Yarn 
-# Sample 
-# Sandal 
-# Sandal Last 
-# Sandal/Cord 
-# Saucer 
-# Scalp Lock 
-# Scat 
-# Scoop 
-# Scoop, Effigy 
-# Scraper 
-# Scraper, End 
-# Scraper, Side 
-# Seed Beater 
-# Shaft 
-# Shaft Smoother 
-# Shawl 
-# Shell 
-# Sherd Artifact 
-# Sherd Disc 
-# Sherd 
-# Sherd, Worked 
-# Shirt 
-# Shovel Blade 
-# Sinew Twister 
-# Sinker 
-# Slab 
-# Sled Runner 
-# Sling 
-# Snare 
-# Snare Stick 
-# Snowshoe 
-# Sock 
-# Soil 
-# Spall 
-# Spear Point 
-# Spindle 
-# Spindle Stick 
-# Spindle Whorl 
-# Split Twig Figurine 
-# Spoon 
-# Stone Artifact 
-# Stone, Polishing 
-# Stopper 
-# Strainer 
-# Strip 
-# Tablita 
-# Textile 
-# Textile Artifact/Plaster Cast 
-# Thread 
-# Throwing Stick 
-# Tinkler 
-# Toggle 
-# Tooth Artifact 
-# Tooth Fragment 
-# Torch 
-# Tray 
-# Tube 
-# Tumbler 
-# Tump Strap 
-# Ulu Blade 
-# Ulu Handle 
-# Unidentified Artifact 
-# Vase 
-# Vegetal Artifact 
-# Vegetal Material 
-# Vessel 
-# Vessel, Effigy 
-# "Weaving Stick"
-# "Whetstone"
-# "Whistle"
-# "Wig/Cord" 
-# "Winged Object"
-# "Wood Artifact"
-# "Wristlet"
-# "Yarn"
-# "Yarn/Cord"
-# "Yucca Knot"
-# "Yucca Knot/Raw Material"]
